8d871c2ab5111f00ac9a3a069884429ff6c1ed4c
Ivan <uvanes21@yandex.ru>
2022-03-09 10:32:01 +0300

    Первый коммит

---
+++ src/main/java/pro/sky/java/lesson19/webemployeestream19/WebEmployeeStream19Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.lesson19.webemployeestream19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEmployeeStream19Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebEmployeeStream19Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/lesson19/webemployeestream19/WebEmployeeStream19ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.lesson19.webemployeestream19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEmployeeStream19ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
